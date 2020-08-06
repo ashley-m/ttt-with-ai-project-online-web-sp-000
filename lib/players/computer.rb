13b29a4636f90078fcb8ad06eabdbf7b05ec7ddd
Ashley Miller <asm2fe@virginia.edu>
2020-08-06 19:01:05 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -4,47 +4,27 @@ module Players
 
     def initialize(tok)
       super(tok)
-      #@game = game
-      #@board = game.board
+      @game = Game.new
+      @board = game.board
     end
 
     def move(board)
-      i = 1
+      i = 5
+      first = 0
       board.cells.each {|x|
         if x == " " # returns next move 
           return i.to_s
         else
-          i+=1
+          if x!=9
+            i += 2
+          else
+            first = 1
+            x = 1
+          end
         end
       }
     end
-    
-    def getNextMove(board, p, depth)
-      {
-        # find available spaces (if theres a winner return the depth(computer)/-depth(player)/0(tie))
-        # 
-        # for each available space set to current player
-        # call getnextmove with new board, next player, depth++
-        #
-      }
 
   end
 
-  class GameTree
-    def sim(player, board, game, depth = 0)
-      valid_moves(board).each{|x|
-      depth +=1
-    }
-    end
-
-    def valid_moves(board)
-      empty = []
-      board.cells.each_with_index{|x, y|
-        if x == " "
-          empty << y+1
-        end
-      }
-      empty
-    end
-  end
 end
